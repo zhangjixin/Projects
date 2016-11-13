e4f55df1072cc678a9379b3f21d10b72a65091f5
jixin zhang <jxin_zhang@163.com>
2016-11-13 21:04:15 +0800

    read xml file & generate excel file

---
+++ SIRegressionReports/SIRegressionReports/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SIRegressionReports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\jixzha\Documents\Visual Studio 2015\Projects\2016-11-10";
+            string excelFileName = @"SIRegressionTestReport.xlsx";
+            var testLST = myXMLReader.xmlReader(filePath);
+            
+            var xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                Console.WriteLine("ERROR: Excel is not available for now.");
+                Console.Read();
+                return;
+            }
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Add(System.Reflection.Missing.Value);
+            Excel.Worksheet xlWorkSheet = xlWorkBook.Worksheets.get_Item(1);
+            int i = 1, j = 1;
+            xlWorkSheet.Cells[1, 1] = "testName";
+            xlWorkSheet.Cells[1, 2] = "outCome";
+            xlWorkSheet.Cells[1, 3] = "startTime";
+            xlWorkSheet.Cells[1, 4] = "endTime";
+            xlWorkSheet.Cells[1, 5] = "duration";
+            foreach (var xlData in testLST)
+            {
+                j = 1;
+                i += 1;
+                xlWorkSheet.Cells[i, j++] = xlData.testName;
+                xlWorkSheet.Cells[i, j++] = xlData.outcom;
+                xlWorkSheet.Cells[i, j++] = xlData.startTime;
+                xlWorkSheet.Cells[i, j++] = xlData.endTime;
+                xlWorkSheet.Cells[i, j++] = xlData.duration;
+            }
+            xlWorkBook.SaveAs(excelFileName);
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();            
+        }
+    }
+}
---
+++ SIRegressionReports/SIRegressionReports/myXMLReader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace SIRegressionReports
+{
+    public class XMLData
+    {
+        public string testName;
+        public string duration;
+        public string startTime;
+        public string endTime;
+        public string outcom;
+        public XMLData()
+        {
+            testName = string.Empty;
+            startTime = string.Empty;
+            endTime = string.Empty;
+            duration = string.Empty;
+            outcom = string.Empty;
+        }
+    }
+    class myXMLReader
+    {
+        public static List<XMLData> xmlReader(string filePath, Boolean pendingFlag = true)
+        {
+            if (File.Exists(filePath))
+            {
+                return xmlFileReader(filePath, pendingFlag);
+            }
+            else if (Directory.Exists(filePath))
+            {
+                List<XMLData> myList = new List<XMLData>();
+                foreach (var file in Directory.GetFiles(filePath, "*", SearchOption.AllDirectories))
+                {
+                    myList.AddRange(xmlFileReader(file));
+                }
+                return myList;
+            }
+            return null;
+        }
+        public static List<XMLData> xmlFileReader(string fileName, Boolean pendingFlag = true)
+        {
+            List<XMLData> myList = new List<XMLData>();
+            XmlTextReader xmlReader = new XmlTextReader(fileName);
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element && xmlReader.Name == "UnitTestResult")
+                {
+                    var xmlData = new XMLData();
+                    xmlData.testName = xmlReader.GetAttribute("testName");
+                    xmlData.duration = xmlReader.GetAttribute("duration");
+                    xmlData.startTime = xmlReader.GetAttribute("startTime");
+                    xmlData.endTime = xmlReader.GetAttribute("endTime");
+                    xmlData.outcom = xmlReader.GetAttribute("outcome");
+                    if (!pendingFlag || xmlData.outcom != "Pending")
+                    {
+                        myList.Add(xmlData);
+                    }
+                }
+            }
+            xmlReader.Close();
+            return myList;
+        }
+    }
+}
